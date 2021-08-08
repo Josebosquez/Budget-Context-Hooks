b8d98cf68236cd3e7b50f6fb9c561796cc998309
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-08-08 14:48:17 -0600

    income array is being displayed.

--- budget-context-hooks/src/components/Lists/IncomeList.js
+++ budget-context-hooks/src/components/Lists/IncomeList.js
@@ -1,12 +1,25 @@
-import React,{useContext}from 'react'
+import React, { useContext } from 'react'
 import { ListsContext } from '../../context/context'
 
 function IncomeList() {
-    const incomeArray = useContext(ListsContext)
-    console.log(incomeArray)
+    const {incomeArray} = useContext(ListsContext)
+
     return (
         <div>
             <h2>Income</h2>
+            <ul>
+                {incomeArray.map((income, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{income.description}</span>
+                            <span>{income.amount}</span>
+                            <button> Delete</button>
+                            <br/>
+                        </li>
+                    );
+                })
+                }
+            </ul>
         </div>
     )
 }
