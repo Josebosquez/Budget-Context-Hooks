943294a98ab3039354fd70aa80c9d6194ed1b837
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-08-08 14:52:06 -0600

    expense list is functional like incomelist.js

--- budget-context-hooks/src/components/Lists/ExpenseList.js
+++ budget-context-hooks/src/components/Lists/ExpenseList.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { ListsContext } from '../../context/context'
 
 function ExpenseList() {
+    const {expenseList} = useContext(ListsContext)
+
     return (
         <div>
             <h2>Expense</h2>
+            <ul>
+                {expenseList.map((expense, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{expense.description}</span>
+                            <span>{expense.amount}</span>
+                            <button> Delete</button>
+                            <br />
+                        </li>
+                    );
+                })
+                }
+            </ul>
         </div>
     )
 }
